cd3e4efc4e6445301a0bfce3152c4d0811b42bf5
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2024-05-02 10:22:06 +0700

    Update
    Successfully testing KNN

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -19,13 +19,23 @@ class KNN:
     def _predict(self, x):
         # calculate the distance
         distances = [self.euclidean_distance(x, x_train) for x_train in self.X_train]
+        # print(f'distances: {distances}')
+        # print(f'argsort_distances: {np.argsort(distances)}')
 
         # get the closest k
         k_indices = np.argsort(distances)[:self.k]
         k_nearest_labels = [self.y_train[i] for i in k_indices]
 
+        # print(f'k_indices: {k_indices}')
+        # print(f'k_nearest_labels: {k_nearest_labels}')
+
         # get the label by majority vote
         most_common = Counter(k_nearest_labels).most_common()
-        return most_common
+        # print(f'most_common: {most_common}')
+        # return k_nearest_labels
+        return most_common[0][0]
+    
+    def _accuracy(self, prediction, target):
+        return (np.sum(prediction == target) / len(target)) * 100
 
 
---
+++ KNN/train.py
@@ -0,0 +1,25 @@
+import numpy as np
+from KNN import KNN
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+from matplotlib.colors import ListedColormap
+cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+iris_dateset = datasets.load_iris()
+X, y = iris_dateset.data, iris_dateset.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# plt.figure()
+# plt.scatter(X[:,2], X[:,3], c=y, cmap=cmap, edgecolors='k', s=20)
+# plt.show()
+
+classifier = KNN(k=5)
+classifier.fit(X_train, y_train)
+predictions = classifier.predict(X_test)
+accuracy = classifier._accuracy(predictions, y_test)
+
+# print(f'k_nearest_labels = {k_nearest_labels}')
+print(f'predictions: {predictions}')
+print(f'target: {y_test}')
+print(f'accuracy: {accuracy}')
