25f0e7a181e324ccb149e3ffc3c406a91e0c0abd
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2024-05-12 12:29:35 +0700

    Update train Naive Bayes

--- Naive Bayes/train.py
+++ Naive Bayes/train.py
@@ -4,14 +4,18 @@ from NaiveBayes import NaiveBayes
 import numpy as np
 
 def accuracy(target, pred):
-    return np.sum(target == pred) / len(target)
+    return (np.sum(target == pred) / len(target)) * 100
 
-X, y = datasets.make_classification(n_samples=1000, n_features=10, n_classes=2, random_state=123)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+# X, y = datasets.make_classification(n_samples=1000, n_features=10, n_classes=2, random_state=123)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+
+iris_dataset = datasets.load_iris()
+X, y = iris_dataset.data, iris_dataset.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 model = NaiveBayes()
 model.fit(X_train, y_train)
 pred = model.predict(X_test)
 acc = accuracy(y_test, pred)
 
-print(acc)
+print(f'Model accuracy = {acc} %')
